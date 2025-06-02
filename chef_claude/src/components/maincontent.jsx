658245a345740a966229a30ed1bd4076cb0f3b08
krudoppler <bkru33@gmail.com>
2025-06-02 18:48:27 -0500

    added ingredients

--- chef_claude/src/components/maincontent.jsx
+++ chef_claude/src/components/maincontent.jsx
@@ -1,16 +1,30 @@
-export default function MainContent(){
-    return (
-        <main>
-            <form className="add-ingredient-form" action="">
-                <input 
-                    aria-label="Add Ingredients"
-                    type="text"
-                    placeholder="e.g. Salt" 
-                />
-                <button>
-                    Add Ingredient
-                </button>
-            </form>
-        </main>
-    )
+export default function MainContent() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+
+  const ingredientsList = ingredients.map((ingredient) => {
+    return <li key={ingredient}>{ingredient}</li>;
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+    console.log(newIngredient);
+    ingredients.push(newIngredient)
+    console.log(ingredients)
+  }
+  return (
+    <main>
+      <form className="add-ingredient-form" onSubmit={handleSubmit}>
+        <input
+          aria-label="Add Ingredients"
+          type="text"
+          placeholder="e.g. Salt"
+          name="ingredient"
+        />
+        <button>Add Ingredient</button>
+      </form>
+      <ul>{ingredientsList}</ul>
+    </main>
+  );
 }
