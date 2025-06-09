abffd6f081a2c7337aff6d5b2dfa311991d854cb
krudoppler <bkru33@gmail.com>
2025-06-09 12:04:36 -0500

    Changed onSubmit to action in the form

--- chef_claude/src/components/maincontent.jsx
+++ chef_claude/src/components/maincontent.jsx
@@ -2,22 +2,20 @@ import React from "react"
 
 export default function MainContent() {
 
-  const [myingredients, setMyIngredients] = React.useState(["Chicken"])
+  const [myingredients, setMyIngredients] = React.useState([])
 
 
   const ingredientsList = myingredients.map((ingredient) => {
     return <li key={ingredient}>{ingredient}</li>;
   });
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const formData = new FormData(event.currentTarget);
+  function addIngredient(formData) {
     const newIngredient = formData.get("ingredient");
     setMyIngredients(prevIngredientList => [...prevIngredientList, newIngredient])
   }
   return (
     <main>
-      <form className="add-ingredient-form" onSubmit={handleSubmit}>
+      <form className="add-ingredient-form" action={addIngredient}>
         <input
           aria-label="Add Ingredients"
           type="text"
